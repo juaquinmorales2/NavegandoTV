14e074564aa80d4d7a908fd7605cd42758a3f3a4
Juaquin Morales <juaquin.moralesbb@gmail.com>
2025-07-08 12:52:11 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 Esto es clave para dominios personalizados
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
